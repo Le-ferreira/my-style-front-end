c3ef709872d5a8cff553be36f8a98819f54180a8
Nicholas Ferreira <nichoas_sc@live.com>
2021-04-10 18:46:51 -0300

    add captcha e bootstrap

--- src/pages/auth/SingIn.tsx
+++ src/pages/auth/SingIn.tsx
@@ -2,46 +2,44 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 import { FiLogIn } from "react-icons/fi";
-import { Form } from "react-bootstrap"
+import { Form, Button } from "react-bootstrap"
 
 
 import '../../styles/pages/auth/SingIn.css'
 
 
 function SingIn() {
-    return (
-        <div id='page-SingIn'>
-            <aside>
-                <div className='content-wrapper'>
-                    {/* <img src={logoImg} alt="Logo Happy" />*/}
-                    <main>
-                        <h1>MyStyle</h1>
-                    </main>
+  return (
+    <div id='page-SingIn'>
+      <aside>
+        <div className='content-wrapper'>
+          {/* <img src={logoImg} alt="Logo Happy" />*/}
+          <main>
+            <h1>MyStyle</h1>
+          </main>
 
-                    <Form>
-                        <Form.Group controlId="formGroupEmail">
-                            <Form.Control type="email" placeholder="Endereço de email" />
-                        </Form.Group>
+          <Form>
+            <Form.Group controlId="formGroupEmail">
+              <Form.Control type="email" placeholder="Endereço de email" />
+            </Form.Group>
 
-                        <Form.Group controlId="formGroupPassword">
-                            <Form.Control type="password" placeholder="Senha" />
-                        </Form.Group>
+            <Form.Group controlId="formGroupPassword">
+              <Form.Control type="password" placeholder="Senha" />
+            </Form.Group>
 
-                        <Link to='app' className='enter-app'>
-                            <p>Entrar</p>
-                        </Link>
+            <Button variant="primary" type="submit" block>
+              Entrar
+            </Button>
 
-                        <Link to='SingUp' className='register-app'>
-                            <FiLogIn color='#4dacb1'/>
-                            <p> Não tenho cadastro </p>
-                        </Link>
-
-                    </Form>
-                    
-                </div>
-            </aside>
+            <Link to='SingUp' className='register-app'>
+              <FiLogIn color='#4dacb1' className="icon" />
+              Não tenho cadastro
+            </Link>
+          </Form>
         </div>
-    )
+      </aside>
+    </div>
+  )
 }
 
 export default SingIn
--- src/pages/auth/SingUp.tsx
+++ src/pages/auth/SingUp.tsx
@@ -1,43 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Form } from "react-bootstrap"
-
+import { Form, Button } from "react-bootstrap"
 import { FiLogIn } from "react-icons/fi";
+import ReCAPTCHA from "react-google-recaptcha";
 
 import '../../styles/pages/auth/SingUp.css'
+const recaptchaRef = React.createRef<ReCAPTCHA>();
+class SingUp extends Component {
+  onChange = (value: any) => {
+    console.log("Captcha value:", value);
+  }
 
-function SingUp() {
+  onSubmit = () => {
+    const recaptchaValue = recaptchaRef.current?.getValue();
+    console.log(recaptchaValue)
+  }
+
+  render() {
     return (
-        <div id="page-SingUp">
+      <div id="page-SingUp">
 
-            <aside>
-                <div className='content-wrapper'>
-                    <main>
-                        <h1>MyStyle</h1>
-                        <p>Register</p>
-                    </main>
-                    <Form.Group controlId="formGroupEmail">
-                        <Form.Control type="email" placeholder="Endereço de email" />
-                    </Form.Group>
+        <aside>
+          <div className='content-wrapper'>
+            <main>
+              <h1>MyStyle</h1>
+              <p>Register</p>
+            </main>
+            <Form onSubmit={this.onSubmit}>
+              <Form.Group controlId="formGroupEmail">
+                <Form.Control type="email" placeholder="Endereço de email" />
+              </Form.Group>
 
-                    <Form.Group controlId="formGroupPassword">
-                        <Form.Control type="password" placeholder="Senha" />
-                    </Form.Group>
+              <Form.Group controlId="formGroupPassword">
+                <Form.Control type="password" placeholder="Senha" />
+              </Form.Group>
 
-                    <Link to='SingUp' className='enter-app'>
-                        <p>Cadastre-se</p>
-                    </Link>
+              <Form.Group controlId="formGroupPassword">
+                <ReCAPTCHA
+                  ref={recaptchaRef}
+                  sitekey="6LfbV5saAAAAAEHimZ9WKlklVPoq9pK_lxyl34OF"
+                  onChange={this.onChange}
+                />
+              </Form.Group>
 
-                    <Link to='/' className='register-app'>
-                        <FiLogIn color='#4dacb1' />
-                        <p> Já possuo cadastro </p>
-                    </Link>
-                </div>
-            </aside>
+              <Button variant="primary" type="submit" block>
+                Cadastre-se
+              </Button>
 
-        </div>
+              <Link to='/' className='register-app'>
+                <FiLogIn color='#4dacb1' className="icon" />
+                Já possuo cadastro
+             </Link>
+            </Form>
+          </div>
+        </aside>
+
+      </div>
     );
+  }
 }
-
 
 export default SingUp
