442c6c6ebdb48f1f7702477d6a15ad793f68b5f2
Nicholas Ferreira <nichoas_sc@live.com>
2021-04-12 20:05:56 -0300

    confirmar senha

--- src/pages/auth/SingUp.tsx
+++ src/pages/auth/SingUp.tsx
@@ -1,19 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, FormEvent } from 'react';
 import { Link } from "react-router-dom";
 import { Form, Button } from "react-bootstrap"
 import { FiLogIn } from "react-icons/fi";
 import ReCAPTCHA from "react-google-recaptcha";
 
 import '../../styles/pages/auth/SingUp.css'
+import axios from 'axios';
 const recaptchaRef = React.createRef<ReCAPTCHA>();
-class SingUp extends Component {
+
+type MyState = { senha: string; email: string; nome: string; confirmar_senha: string };
+class SingUp extends Component<{}, MyState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      email: '',
+      senha: '',
+      confirmar_senha: '',
+      nome: 'Nicholas'
+    };
+  }
+
   onChange = (value: any) => {
     console.log("Captcha value:", value);
   }
 
-  onSubmit = () => {
+  onSubmit = async (e: any) => {
+    e.preventDefault()
     const recaptchaValue = recaptchaRef.current?.getValue();
-    console.log(recaptchaValue)
+
+    await axios.post(`http://localhost:3000/auth/signup`, {
+      ...this.state
+    }, { 
+      headers: { recaptcha: recaptchaValue }
+    }).catch(err => {
+      console.error(err)
+    }).then(res => {
+      console.log(res)
+    })
   }
 
   render() {
@@ -28,11 +51,15 @@ class SingUp extends Component {
             </main>
             <Form onSubmit={this.onSubmit}>
               <Form.Group controlId="formGroupEmail">
-                <Form.Control type="email" placeholder="Endereço de email" />
+                <Form.Control type="email" placeholder="Endereço de email" onChange={(e) => this.setState({ email: e.target.value })} />
               </Form.Group>
 
               <Form.Group controlId="formGroupPassword">
-                <Form.Control type="password" placeholder="Senha" />
+                <Form.Control type="password" placeholder="Senha" onChange={(e) => this.setState({ senha: e.target.value })} />
+              </Form.Group>
+
+              <Form.Group controlId="formGroupPassword">
+                <Form.Control type="password" placeholder="Confirmar Senha" onChange={(e) => this.setState({ confirmar_senha: e.target.value })} />
               </Form.Group>
 
               <Form.Group controlId="formGroupPassword">
